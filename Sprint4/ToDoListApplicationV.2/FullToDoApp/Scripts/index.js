d7f3dca3374df2b1ecdf05777868f6e809c680c3
Andrew Essam <andrewessam-2007@hotmail.com>
2023-06-04 16:21:49 +0300

    updated

--- Sprint4/ToDoListApplicationV.2/FullToDoApp/Scripts/index.js
+++ Sprint4/ToDoListApplicationV.2/FullToDoApp/Scripts/index.js
@@ -1,5 +1,6 @@
 let taskList = [];
-
+let oldyTaskName;
+let oldyOrder;
 let addTask = function () {
   const taskName = document.getElementById(`taskName`).value;
   const priority = document.getElementById(`priority`).value;
@@ -18,7 +19,7 @@ let addTask = function () {
 };
 
 let validate = function (taskObject) {
-  if (taskObject.Name && taskObject.order !== null) {
+  if (taskObject.Name && taskObject.order ) {
     return true;
   } else {
     return false;
@@ -30,12 +31,13 @@ let addtoArray = function (taskObject) {
 };
 
 let renderTable = function () {
+  let clicked = false;
   let tb = document.getElementById(`myTable`);
   let tbody = "";
   for (let i = 0; i < taskList.length; i++) {
     tbody += `<tr> <td>${i + 1}</td> <td>${taskList[i].Name}</td> <td>${
       taskList[i].order
-    }</td> <td><button onclick= "editText(${i})"> edit</button></td>  <td><button onclick="deleteRow(${i})">Delete</button></td></tr>`;
+    }</td> <td><button onclick= "editText(${i},${clicked})" id="editButton${i}"> edit</button></td>  <td><button onclick="deleteRow(${i})" id="deleteButton${i}">Delete</button></td></tr>`;
   }
   document.getElementById("myTable").innerHTML = tbody;
 };
@@ -56,5 +58,48 @@ sortTable = function () {
   renderTable();
 };
 
+//Edit Button
+editText = function (i, clicked) {
+  oldyTaskName = oldValue(taskList[i].Name);
+  oldyOrder = oldValue2(taskList[i].order)
+  clicked = true;
 
+  if (clicked === true) {
+    taskList[i].Name = `<input id="editInput${i}">`;
+    taskList[i].order = `<input id="editInput2${i}">`;
+  }
+  renderTable();
+  convertButtons(i, oldValue);
+};
 
+// Convert Buttons to save and Cancel
+convertButtons = function (i, oldValue) {
+  let editButton = document.getElementById(`editButton${i}`);
+  let deleteButton = document.getElementById(`deleteButton${i}`);
+  editButton.outerHTML = `<button onclick="saveButton(${i})">Save</button>`;
+  deleteButton.outerHTML = `<button onclick="cancelButton(${i},${oldValue})">Cancel</button>`;
+};
+
+// Save Button
+let saveButton = function (i) {
+  let editedDoc = document.getElementById(`editInput${i}`).value;
+  taskList[i].Name = editedDoc;
+  console.log(taskList);
+  renderTable();
+};
+// Cancel Button
+
+let cancelButton = function (i) {
+  let a = oldValue();
+  taskList[i].Name = oldyTaskName;
+  taskList[i].order = oldyOrder;
+  renderTable();
+};
+// function to return old value before it has been edited used for cancel button
+let oldValue = function (q) {
+  return q;
+};
+
+let oldValue2 = function (q) {
+  return q;
+};
